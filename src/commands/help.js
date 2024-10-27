18c34389e1bf0f9325ba334e961e50608bdffdfd
Tabea <dargatztabea@gmail.com>
2024-10-27 23:32:48 +0100

    updated help command info

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,10 +11,15 @@ To roll a check, use the following command. You can specify as many of the optio
       /roll             rolls a d20
          [sides]         specifies the amount of sides
          [times]        specifies how many dice should be rolled
-         [skill]           automatically adds the skill modifier (i.e. +2) that is registered for this person.
+         [skill]           automatically adds the skill modifier (i.e. +2) that is registered for this person
     
 **STATS**
 The bot will keep track of your d20 rolls and allows you to look at some semi interesting stats, I guess.
-      /stats        shows a statistics of your crit fails, nat 20s, and skill usage.
-      /reset        resets all statistics back to 0 for your character.
+      /stats        shows a statistics of your crit fails, nat 20s, and skill usage
+      /reset        resets all statistics for your character
+
+**MISC**
+Other commands
+      /help         explains all commands
+      /invite        creates an invite link for this bot
 `;
