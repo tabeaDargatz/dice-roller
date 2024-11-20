3abf364226bed47bc5340f63f80b68c68288c9dc
Tabea <dargatztabea@gmail.com>
2024-11-20 21:58:15 +0100

    updated CharacterDetails table so it can hold all relevant info for the api call

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS Statistic (PlayerName TEXT, Measurement TEXT, Count INTEGER, PRIMARY KEY (PlayerName, Measurement));
-INSERT INTO Statistic (PlayerName, Measurement, Count)
-VALUES ('Cara', 'Nat20s', 0);
-
 CREATE TABLE IF NOT EXISTS SkillModifiers (PlayerName TEXT, Skill TEXT, Modifier INTEGER, PRIMARY KEY (PlayerName, Skill));
-INSERT INTO SkillModifiers (PlayerName, Skill, Modifier)
-VALUES ('Cara', 'Deception', 2);
+CREATE TABLE IF NOT EXISTS CharacterDetails (PlayerName TEXT, Age INTEGER, Gender TEXT, Height TEXT, Weight TEXT, Hair TEXT, Eyes TEXT, Skin TEXT, Alignment TEXT, Faith TEXT,
+Backstory TEXT, Personality TEXT, Strength INTEGER, Dexterity INTEGER, Constitution INTEGER, Intelligence INTEGER, Wisdom INTEGER, Charisma INTEGER, Hp INTEGER, Ac INTEGER, 
+Speed INTEGER, InitiativeBonus INTEGER, SpellAtkBonus INTEGER, SpellMod INTEGER, ClassFeatures TEXT, SpeciesTraits TEXT, Actions TEXT, BonusActions TEXT, Level INTEGER, 
+Class TEXT, Race TEXT,  PRIMARY KEY (PlayerName));
 
-
-CREATE TABLE IF NOT EXISTS CharacterDetails (PlayerName TEXT, Age INTEGER, Gender TEXT, Height TEXT, Weight TEXT, Hair TEXT, Eyes TEXT, Skin TEXT, Alignment TEXT, Faith TEXT,  PRIMARY KEY (PlayerName));
-INSERT INTO CharacterDetails (PlayerName, Age, Gender, Height, Weight, Hair, Eyes, Skin, Alignment, Faith)
-VALUES ('Cara', 20, 'Female', '160cm', '55kg', 'long, brown', 'green', 'pale', 'chaotic good','none');
+INSERT INTO CharacterDetails (PlayerName, Age, Gender, Height, Weight, Hair, Eyes, Skin, Alignment, Faith, Backstory, Personality, Strength, Dexterity, Constitution, 
+Intelligence, Wisdom, Charisma, Hp, Ac, Speed, InitiativeBonus, SpellAtkBonus, SpellMod, ClassFeatures, SpeciesTraits, Actions, BonusActions, Level, Class, Race)
+VALUES ('Cara', 20, 'Female', '160cm', '55kg', 'long, brown', 'green', 'pale', 'chaotic good','none','She is a cool bean', 'Awesome personality', 11,15,16,10,10,18,
+19,13,30,2,4,4,'Bardic Inspiration blabla', 'humans get heroic inspiration after long rest',
+'Attack,Dash,Disengage,Dodge,Grapple,Help,Hide,Improvise,Influence,Magic,Ready,Search,Shove,Study,Utilize','Can give bardic inspiration',2,'Bard','Human');
