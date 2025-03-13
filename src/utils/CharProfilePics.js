408d5ba2bb6a05e32eb23308446c4ca25c843e5d
Tabea <dargatztabea@gmail.com>
2025-03-13 23:10:00 +0100

    updated Cara img, added Ainikki img

--- src/utils/CharProfilePics.js
+++ src/utils/CharProfilePics.js
@@ -1,15 +1,13 @@
 export function getThumbnail(characterName) {
   switch (characterName) {
     case 'Cara':
-      return 'https://i.imgur.com/0PXsuAq.png';
+      return 'https://imgur.com/gyfLWmd';
     case 'Xulle':
       return 'https://i.imgur.com/MNMaFar.png';
     case 'Ruut':
       return 'https://i.imgur.com/x2dioxD.png';
-    case 'Time Keeper':
-      return 'https://i.imgur.com/UfPtgAD.png';
-    case 'Volpi':
-      return 'https://i.imgur.com/yd9muuF.jpeg';
+    case 'Ainikki':
+      return 'https://imgur.com/z8tZMJb';
     case 'The Almighty DnD God (DM)':
       return 'https://i.imgur.com/4y4N3Xh.png';
     default:
