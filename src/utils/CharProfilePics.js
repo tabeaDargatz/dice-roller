be66386399349d81eb13278b6b44976aa5aec718
Tabea <dargatztabea@gmail.com>
2025-03-13 23:11:18 +0100

    added png ending

--- src/utils/CharProfilePics.js
+++ src/utils/CharProfilePics.js
@@ -1,13 +1,13 @@
 export function getThumbnail(characterName) {
   switch (characterName) {
     case 'Cara':
-      return 'https://imgur.com/gyfLWmd';
+      return 'https://imgur.com/gyfLWmd.png';
     case 'Xulle':
       return 'https://i.imgur.com/MNMaFar.png';
     case 'Ruut':
       return 'https://i.imgur.com/x2dioxD.png';
     case 'Ainikki':
-      return 'https://imgur.com/z8tZMJb';
+      return 'https://imgur.com/z8tZMJb.png';
     case 'The Almighty DnD God (DM)':
       return 'https://i.imgur.com/4y4N3Xh.png';
     default:
